90a0402fd764a73577cde1682b66b27fc697e936
Ioana David <40031719+ioanaddd@users.noreply.github.com>
2018-06-08 12:26:40 +0100

    progress on the registration form
    also an attempt to create a footer and a header,  put all css  files  together but they need to be connected accordingly

---
+++ Disertation-master/includes/footer.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<head>
+
+<link rel="stylesheet" type="text/css" href="./styles/allstyles">
+</head>
+
+<body>
+	<footer id="footer">
+		ioana and irina 2018
+	</footer>
+</body>
+</hmtl>
--- Disertation-master/register.php
+++ Disertation-master/register.php
@@ -1,6 +1,27 @@
-<?php include('connect.php');
+
+<?php
+session_start();
+
+if(isset($_POST["Register"])){
+
+include 'connect.php';
+
+$conn=connect();
+	
+
+$FirstName= $_POST["FirstName"];
+$LastName= $_POST["LastName"];
+$Password= $_POST["Password"];
+$Username= $_POST["Username"];
+$Email=$_POST["Email"];
+
+$sql = "INSERT INTO users (FirstName, LastName, Username, Password,Email)VALUES ('$FirstName','$LastName','$Username','$Password','$Email');";
+$handle = $conn->prepare($sql);
+$handle->execute();
+$conn = null;
+}
 ?>
-<DOCTYPE! html>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Register</title>
@@ -21,12 +42,12 @@
 <div class="registration-title">
     <h1>Create an account</h1>
 </div>
-    <form id="registration-form" class="form" action="register.inc.php" method="post" >
-	  <input type="text"class="form-control" name="first" placeholder="Firstname">
-	  <input type="text" class="form-control" name="last" placeholder="Lastname">
-	  <input type="text" class="form-control" placeholder="Username"  name="username"  required />
-      <input type="email" class="form-control" placeholder="Email"  name="email" required />
-      <input type="password" class="form-control" placeholder="Password" name="password_1" autocomplete="new-password" required />
+    <form id="registration-form" class="form" action="login.php" method="post" >
+	  <input type="text"class="form-control" name="FirstName" placeholder="Firstname"  required/>
+	  <input type="text" class="form-control" name="LastName" placeholder="Lastname" required/>
+	  <input type="text" class="form-control" placeholder="Username"  name="Username"  required />
+      <input type="email" class="form-control" placeholder="Email"  name="Email" required />
+      <input type="password" class="form-control" placeholder="Password" name="Password" autocomplete="new-password" required />
       <!--<input type="password"class="form-control" placeholder="Confirm Password" name="password_2" autocomplete="new-password" required />-->
       <!--<div class="avatar"><label>Select your avatar: </label><input type="file" name="avatar" accept="image/*" required /></div>-->
       <input type="submit" class="form-control submit" value="Register" name="register" />
